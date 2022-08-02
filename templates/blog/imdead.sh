b03d6cbd0a72d6f64db8d8a5b8087ef4bcad57b8
CPunch <sethtstubbs@gmail.com>
2022-08-02 00:13:16 -0500

    Deadswitch: I am dumb

--- templates/blog/imdead.sh
+++ templates/blog/imdead.sh
@@ -2,18 +2,17 @@
 
 cd $HOME/deadman
 
-postPatch='dead.patch'
-pageName='openpunk/content/pages/dead.md'
+postPatch='../dead.patch'
+pageName='content/pages/dead.md'
 currDate=$(date '+%Y-%m-%d')
 
 git clone git@github.com:CPunch/openpunk.git
-git am postPatch
-
-# replace our --DATE-- with the current date
-sed -i 's/--DATE--/'$currDate'/g' $pageName
 
 # commit & push the post
 cd openpunk
+git am postPatch
+# replace our --DATE-- with the current date
+sed -i 's/--DATE--/'$currDate'/g' $pageName
 git add .
 git commit -m "DeadSwitch: No response from CPunch in 14 days, posting dead.md"
 git push --force
